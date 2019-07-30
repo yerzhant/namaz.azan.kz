af2f7d15cdc203124133efb9ae0abc483482070d
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2019-07-30 16:52:40 +0600

    Added YouTube pausing in background

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,38 @@ class NamazApp extends StatelessWidget {
     FlutterStatusbarcolor.setStatusBarColor(Color(0xF01BA2DD));
     return MaterialApp(
       title: 'Намаз',
-      home: HomePage(),
+      home: _HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class _HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<_HomePage> with WidgetsBindingObserver {
   final _controller = Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.paused) {
+      var c = await _controller.future;
+      c.evaluateJavascript('player.pauseVideo()');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
